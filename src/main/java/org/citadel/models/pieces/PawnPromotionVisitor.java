ba64fa6735b4ce126132be3fcb6d7f8429adb5b3
PabloCarroza <pablo.carroza.ext@vidacamara.cl>
2025-08-28 17:31:26 -0400

    save

--- src/main/java/org/citadel/models/pieces/PawnPromotionVisitor.java
+++ src/main/java/org/citadel/models/pieces/PawnPromotionVisitor.java
@@ -1,6 +1,7 @@
 package org.citadel.models.pieces;
 
 public class PawnPromotionVisitor implements PieceVisitor {
+
     private boolean isPromoted = false;
 
     public boolean isPromoted() {
@@ -9,14 +10,31 @@ public class PawnPromotionVisitor implements PieceVisitor {
 
     @Override
     public void visit(Pawn pawn) {
-        // Accedemos al estado de promoción del peón
         isPromoted = pawn.isPromoted();
     }
 
-    // Para cualquier otra pieza, la promoción no es aplicable.
-    @Override public void visit(King king) {}
-    @Override public void visit(Queen queen) {}
-    @Override public void visit(Rook rook) {}
-    @Override public void visit(Bishop bishop) {}
-    @Override public void visit(Knight knight) {}
+    @Override
+    public void visit(King king) {
+        // Does nothing
+    }
+
+    @Override
+    public void visit(Queen queen) {
+        // Does nothing
+    }
+
+    @Override
+    public void visit(Rook rook) {
+        // Does nothing
+    }
+
+    @Override
+    public void visit(Bishop bishop) {
+        // Does nothing
+    }
+
+    @Override
+    public void visit(Knight knight) {
+        // Does nothing
+    }
 }
