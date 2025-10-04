38acc767d13da5948e4dc146d102aaa693636bb4
PabloCarroza <pablo.carroza.ext@vidacamara.cl>
2025-10-04 01:37:35 -0300

    save

--- src/main/java/org/citadel/views/console/SpecialMovesView.java
+++ src/main/java/org/citadel/views/console/SpecialMovesView.java
@@ -1,13 +1,13 @@
 package org.citadel.views.console;
 
-import org.citadel.controllers.local.LocalSpecialMovementsController;
+import org.citadel.controllers.SpecialMovementsController;
 import org.citadel.views.console.menus.SpecialMovesMenu;
 
 import java.util.List;
 
 public class SpecialMovesView {
 
-    public void interact(List<LocalSpecialMovementsController> controllers) {
+    public void interact(List<SpecialMovementsController> controllers) {
         assert controllers != null;
         new SpecialMovesMenu(controllers).execute();
     }
