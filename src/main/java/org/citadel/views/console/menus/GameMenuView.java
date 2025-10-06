3b5a46ed1d239ecc909660e0427eb69673591307
PabloCarroza <pablo.carroza.ext@vidacamara.cl>
2025-10-06 16:15:18 -0300

    save

--- src/main/java/org/citadel/views/console/menus/GameMenuView.java
+++ src/main/java/org/citadel/views/console/menus/GameMenuView.java
@@ -7,15 +7,10 @@ public class GameMenuView extends Menu {
 
     public GameMenuView(GameController gameController, MenuContext context) {
         super("MENÚ PRINCIPAL");
-        Menu historyMenu = new Menu("OPCIONES DE HISTORIAL");
-        historyMenu.setParent(this);
-        historyMenu.add(new MenuItem("HISTORIAL DE PARTIDAS", new ShowHistoryCommand(gameController)));
-        historyMenu.add(new MenuItem("HISTORIAL DE JUGADAS", new GameHistoryCommand(gameController)));
-        historyMenu.add(new MenuItem("VOLVER", new BackCommand(context)));
         add(new MenuItem("INICIAR NUEVA PARTIDA", new InGameCommand(gameController)));
         add(new MenuItem("CARGAR PARTIDA", new LoadGameCommand(gameController)));
         add(new MenuItem("GUARDAR PARTIDA", new SaveGameCommand(gameController)));
-        add(new MenuItem("HISTORIAL", new OpenMenuCommand(context, historyMenu)));
+        add(new MenuItem("HISTORIAL", new OpenMenuCommand(context, new HistoryMenuView(gameController, context))));
         add(new MenuItem("SALIR DE LA PARTIDA", new ExitCommand(gameController)));
     }
 }
---
+++ src/main/java/org/citadel/views/console/menus/HistoryMenuView.java
@@ -0,0 +1,17 @@
+package org.citadel.views.console.menus;
+
+import org.citadel.controllers.GameController;
+import org.citadel.views.console.commands.game.BackCommand;
+import org.citadel.views.console.commands.game.GameHistoryCommand;
+import org.citadel.views.console.commands.game.ShowHistoryCommand;
+
+public class HistoryMenuView extends Menu {
+
+    public HistoryMenuView(GameController gameController, MenuContext context) {
+        super("MENÚ PRINCIPAL");
+        setParent(this);
+        add(new MenuItem("HISTORIAL DE PARTIDAS", new ShowHistoryCommand(gameController)));
+        add(new MenuItem("HISTORIAL DE JUGADAS", new GameHistoryCommand(gameController)));
+        add(new MenuItem("VOLVER", new BackCommand(context)));
+    }
+}
