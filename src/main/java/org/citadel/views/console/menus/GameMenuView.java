4384b8f23a5cf3ac1d23ad7115b856c137178f26
PabloCarroza <pablo.carroza.ext@vidacamara.cl>
2025-10-06 09:39:06 -0300

    save

--- src/main/java/org/citadel/views/console/menus/GameMenuView.java
+++ src/main/java/org/citadel/views/console/menus/GameMenuView.java
@@ -9,10 +9,10 @@ public class GameMenuView extends Menu {
 
     public GameMenuView(GameController gameController) {
         super("MENÚ PRINCIPAL");
-        this.add(new MenuItem("[1] INICIAR PARTIDA", new InGameCommand(gameController)));
-        this.add(new MenuItem("[2] CARGAR PARTIDA", new LoadGameCommand(gameController)));
-        this.add(new MenuItem("[3] GUARDAR PARTIDA", new SaveGameCommand(gameController)));
-        this.add(new MenuItem("[4] HISTORIAL DE PARTIDAS", new ShowHistoryCommand(gameController)));
-        this.add(new MenuItem("[5] SALIR DEL JUEGO", new ExitCommand(gameController)));
+        add(new MenuItem("[1] INICIAR PARTIDA", new InGameCommand(gameController)));
+        add(new MenuItem("[2] CARGAR PARTIDA", new LoadGameCommand(gameController)));
+        add(new MenuItem("[3] GUARDAR PARTIDA", new SaveGameCommand(gameController)));
+        add(new MenuItem("[4] HISTORIAL DE PARTIDAS", new ShowHistoryCommand(gameController)));
+        add(new MenuItem("[5] SALIR DEL JUEGO", new ExitCommand(gameController)));
     }
 }
